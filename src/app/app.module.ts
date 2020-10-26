90813e5f29428f0a23b206be7ca63f7ac208b71a
oletendart <letendartolivia@gmail.com>
2020-10-26 14:23:12 +0100

    Modificate component navigation + remove component header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,10 @@ import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
 import { NavigationComponent } from "./components/navigation/navigation.component";
-import { HeaderComponent } from "./components/header/header.component";
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [
-    AppComponent,
-    HelloComponent,
-    NavigationComponent,
-    HeaderComponent
-  ],
+  declarations: [AppComponent, HelloComponent, NavigationComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
