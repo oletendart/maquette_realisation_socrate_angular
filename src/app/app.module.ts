8e8bd604d876f1058e0e094e3127639fd6ad6b27
oletendart <letendartolivia@gmail.com>
2020-10-26 14:19:59 +0100

    Create component header + insert component in the file app.module.ts + app.component.jtml

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,16 @@ import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
 import { NavigationComponent } from "./components/navigation/navigation.component";
+import { HeaderComponent } from "./components/header/header.component";
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, NavigationComponent],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    NavigationComponent,
+    HeaderComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
