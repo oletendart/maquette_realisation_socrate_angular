df3f3e90bef6a27666b5b8a436a4272c9bea3a34
oletendart <letendartolivia@gmail.com>
2020-10-27 14:46:23 +0100

    Create component know + create html in the file know.component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,16 @@ import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { NavigationComponent } from "./components/navigation/navigation.component";
 import { HeaderComponent } from "./components/header/header.component";
+import { KnowComponent } from "./components/know/know.component";
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, NavigationComponent, HeaderComponent],
+  declarations: [
+    AppComponent,
+    NavigationComponent,
+    HeaderComponent,
+    KnowComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
