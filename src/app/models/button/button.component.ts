bc45dbc41fef34fe4519f16b3ae45cf17ba6bb63
oletendart <letendartolivia@gmail.com>
2020-11-26 12:10:36 +0100

    remove the button

--- src/app/models/button/button.component.ts
+++ src/app/models/button/button.component.ts
@@ -1,9 +1,4 @@
-import { Component, OnInit, Input, EventEmitter, Output } from "@angular/core";
-enum ButtonType {
-  Primary = "Primary",
-  Secondary = "Secondary",
-  Warning = "Warning"
-}
+import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-button",
@@ -11,23 +6,6 @@ enum ButtonType {
   styleUrls: ["./button.component.css"]
 })
 export class ButtonComponent implements OnInit {
-  @Input() text: string;
-  @Input() width: number;
-  @Input() btnType: string;
-
-  @Output() click = new EventEmitter();
-
-  get buttonType() {
-    switch (this.buttonType) {
-      case ButtonType.Primary:
-        return "primary-btn";
-      case ButtonType.Secondary:
-        return "secondary-btn";
-      case ButtonType.Warning:
-        return "warning-btn";
-    }
-  }
-
   constructor() {}
 
   ngOnInit() {}
